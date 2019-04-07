6be354997bf97c8bf9805d684a73b3ebf4589641
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-04-07 12:47:24 +0200

    inintial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function App() {
+  return (
+    <header>
+      <h1>This is React!</h1>
+    </header>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
