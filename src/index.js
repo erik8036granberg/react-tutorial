cfcebbf21dcc0d1d9afa52d5b5bfaed247dd1952
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-04-07 13:38:44 +0200

    del 2

--- src/index.js
+++ src/index.js
@@ -2,11 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const Footer = () => {
+  return <footer>&copy; 2018</footer>;
+};
+
 function App() {
+  return (
+    <div id="App">
+      <Header />
+      <Person />
+      <Person />
+      <Person />
+      <Person />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
   return (
     <header>
       <h1>This is React!</h1>
     </header>
+  );
+}
+
+function Person() {
+  return (
+    <article>
+      <h1>Jonas</h1>
+      <p>Hi mom!</p>
+    </article>
   );
 }
 
